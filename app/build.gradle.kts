71acd124710327e48658894c58d1ffd909e3aea0
Mo06-exe <libitone@gmail.com>
2025-08-30 00:43:47 +0200

    Firebase works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +42,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("ai.picovoice:porcupine-android:3.0.1")
+    implementation("com.google.firebase:firebase-bom:34.2.0")
+    implementation("com.google.firebase:firebase-analytics:23.0.0")
 }

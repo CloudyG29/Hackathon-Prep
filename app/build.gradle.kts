3a2be6ff0f5650d96d917eac0659d6fb557d819e
Mo06-exe <libitone@gmail.com>
2025-08-29 23:08:16 +0200

    Added the wake word function, the app triggers the distress function by keyword "Guardian Help", still adding more functionality to the ai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("ai.picovoice:porcupine-android:3.0.1")
 }
